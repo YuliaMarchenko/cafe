aaa75330446abb2f02493b9427c495e784858d3a
Yulia Marchenko <yuyakovleva11@gmail.com>
2022-08-27 20:47:38 +0200

    ..

--- src/main/java/com/telran/FileHelper.java
+++ src/main/java/com/telran/FileHelper.java
@@ -3,7 +3,6 @@ package com.telran;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
 public class FileHelper {
     public static List<String> readByLine(String path) {
